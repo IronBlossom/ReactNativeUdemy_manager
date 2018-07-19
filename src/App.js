d92762747013ebd33f8b9519ca802032e0e1500a
Ishtiaq Mahmood Amin <imamin@ael.net>
2018-07-19 20:44:37 +0600

    After Finishing: 108 More on Boilerplate Setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { View, Text, } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAXSPSo7Gk7HiCcasXukFGA7h0ANVEJukY',
+      authDomain: 'manager-4a74e.firebaseapp.com',
+      databaseURL: 'https://manager-4a74e.firebaseio.com',
+      projectId: 'manager-4a74e',
+      storageBucket: '',
+      messagingSenderId: '952806959764'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text> textInComponent </Text>
         </View>
